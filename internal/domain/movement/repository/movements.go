efce0a050c65bca23876f9691174fb93802af36e
silvioubaldino <silvioubaldino@gmail.com>
2025-08-19 08:13:20 -0300

    Fix update movement to update subcategory

--- internal/domain/movement/repository/movements.go
+++ internal/domain/movement/repository/movements.go
@@ -207,6 +207,10 @@ func setNewFields(strategy strategy) (strategy, error) {
 		strategy.originalMovement.CategoryID = strategy.newMovement.CategoryID
 		updated = true
 	}
+	if strategy.newMovement.SubCategoryID != nil {
+		strategy.originalMovement.SubCategoryID = strategy.newMovement.SubCategoryID
+		updated = true
+	}
 	if strategy.newMovement.Amount != 0 && strategy.newMovement.Amount != strategy.originalMovement.Amount {
 		strategy.updateStrategies = updateStrategyDifferentAmount
 		strategy.originalMovement.Amount = strategy.newMovement.Amount
