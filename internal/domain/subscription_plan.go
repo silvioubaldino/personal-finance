ceb8410fb37102013ece54bc0c31765008844b51
silvioubaldino <silvioubaldino@gmail.com>
2026-05-21 00:09:15 -0400

    Fix subscription plan json

--- internal/domain/subscription_plan.go
+++ internal/domain/subscription_plan.go
@@ -1,11 +1,11 @@
 package domain
 
 type SubscriptionPlan struct {
-	ID            string
-	Name          string
-	Price         float64
-	Currency      string
-	Frequency     int
-	FrequencyType string
-	IsActive      bool
+	ID            string  `json:"id"`
+	Name          string  `json:"name"`
+	Price         float64 `json:"price"`
+	Currency      string  `json:"currency"`
+	Frequency     int     `json:"frequency"`
+	FrequencyType string  `json:"frequency_type"`
+	IsActive      bool    `json:"is_active"`
 }
